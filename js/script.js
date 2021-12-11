214fba5e8f17535a4d993171c2a755df26b8acf6
mmUggio <antoniofbove@gmail.com>
2021-12-11 23:16:17 +0100

    milestones 2-3

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
- // milestone 1
+// milestone 2-3
+
+/* 
+ho deciso di cambiare l'algoritmo per sfruttare la
+possibilità di cumulare testo di append, che non 
+avevamo a disposizione (credo) con console.log
+
+questo mi sembra più compatto (anche se forse meno
+auto-esplicativo)
+*/
+
+const ul = document.querySelector('ul.list');
+
 for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log('FizzBuzz');
-  } else if (i % 3 === 0) {
-    console.log('Fizz');
-  } else if (i % 5 === 0) {
-    console.log('Buzz');
+  const li = document.createElement('li');
+  li.classList.add('box');
+
+  if (i % 3 !== 0 && i % 5 !== 0) {
+    li.append(`${i}`);
   } else {
-    console.log(`${i}`);
+    if (i % 3 === 0) {
+    li.append('Fizz');
+    li.classList.add('fizz');
+    }
+    if (i % 5 === 0) {
+    li.append('Buzz');
+    li.classList.add('buzz');
+    }
   }
+  ul.append(li);
 }
